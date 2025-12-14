e8f3ca058a02d4ccc273017234040b17a9fbe68e
Sanket <sanketpachghare2001@gmail.com>
2025-12-14 15:54:54 +0530

    :: Kafka with bootstrap server , producer

--- src/main/java/com/sp/kafka/KafkaController.java
+++ src/main/java/com/sp/kafka/KafkaController.java
@@ -15,28 +15,8 @@ public class KafkaController {
     private final Producer kafkaProducer;
 
     @GetMapping("/send")
-    public ResponseEntity<String> sendMessage(@RequestParam String message){
+    public ResponseEntity<String> sendMessage(@RequestParam String message) {
         kafkaProducer.sendMessage(message);
-        return ResponseEntity.ok("Message sent successfully");
-    }
-
-
-    @GetMapping("/sendAsync")
-    public ResponseEntity<String> sendMessageAsync(@RequestParam String message){
-        kafkaProducer.sendMessageWithCallback(message);
-        return ResponseEntity.ok("Message sent successfully");
-    }
-
-
-    @GetMapping("/sendAsyncMultiple")
-    public ResponseEntity<String> sendMessageAsyncMultiple(@RequestParam String message, @RequestParam boolean isBatch){
-        kafkaProducer.sendMessageWithCallbackMultiple(message, isBatch);
-        return ResponseEntity.ok("Message sent successfully");
-    }
-
-    @GetMapping("/sendKeyValue")
-    public ResponseEntity<String> sendMessageKeyValue(@RequestParam String key, @RequestParam String value){
-        kafkaProducer.sendMessageWithKey(key, value);
         return ResponseEntity.ok("Message sent successfully");
     }
 
--- src/main/java/com/sp/kafka/Producer.java
+++ src/main/java/com/sp/kafka/Producer.java
@@ -11,9 +11,9 @@ import java.util.Properties;
 @Service
 public class Producer {
 
-   private final Logger logger = LoggerFactory.getLogger(Producer.class);
+    private final Logger logger = LoggerFactory.getLogger(Producer.class);
 
-    public void sendMessage(String message){
+    public void sendMessage(String message) {
         try {
             Properties props = new Properties();
             // create properties
@@ -30,143 +30,9 @@ public class Producer {
             producer.flush();
             producer.close();
 
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void sendMessageWithCallback(String message){
-        try {
-            Properties props = new Properties();
-            // create properties
-            props.put("bootstrap.servers", "127.0.0.1:9092");
-            props.put("key.serializer", StringSerializer.class.getName());
-            props.put("value.serializer", StringSerializer.class.getName());
-            // create producer
-            KafkaProducer<String, String> producer = new KafkaProducer<>(props);
-
-
-            ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", message);
-            producer.send(record,new Callback() {
-                @Override
-                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-                    // executes every time a record successfully sent or throw an exception
-                    if(e==null){
-                        logger.info("Received new metadata \n" +
-                                "Topic : {} \n"+
-                                "Partition : {} \n"+
-                                "Offset : {} \n"+
-                                "Timestamp : {} \n", recordMetadata.topic(),recordMetadata.partition(), recordMetadata.offset(), recordMetadata.timestamp());
-                    }
-                    else{
-                        logger.error("Error while sending message, {}" ,e.getMessage(), e);
-                    }
-                }
-            });
-            logger.info("Send message to Kafka Async");
-            logger.info(message);
-            producer.flush();
-            producer.close();
-
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    // here if we are producing 10 records still this record will go to same partition , as default partition will use
-    public void sendMessageWithCallbackMultiple(String message, boolean batch){
-        try {
-            Properties props = new Properties();
-            // create properties
-            props.put("bootstrap.servers", "127.0.0.1:9092");
-            props.put("key.serializer", StringSerializer.class.getName());
-            props.put("value.serializer", StringSerializer.class.getName());
-            if(batch){
-                props.setProperty("batch.size", "400");
-            // not recommended
-//                props.setProperty("partitioner.class", RoundRobinPartitioner.class.getName());
-            }
-            // create producer
-            KafkaProducer<String, String> producer = new KafkaProducer<>(props);
-            if(!batch)produceBatch(producer, message);
-            else{
-                // here we are sending message as batch with some time interval
-                for(int j = 0; j < 10; j++){
-                    produceBatch(producer, message);
-                    try{
-                        Thread.sleep(5000);
-                    }
-                    catch(InterruptedException e){
-                        logger.error(e.getMessage(),e);
-                    }
-                }
-            }
-            producer.flush();
-            producer.close();
-
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    private void produceBatch(KafkaProducer<String,String> producer, String message){
-        for (int i = 0; i < 30; i++) {
-            ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", message+ " : "+ i);
-            producer.send(record, new Callback() {
-                @Override
-                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-                    // executes every time a record successfully sent or throw an exception
-                    if (e == null) {
-                        logger.info("Received new metadata \n" +
-                                "Topic : {} \n" +
-                                "Partition : {} \n" +
-                                "Offset : {} \n" +
-                                "Timestamp : {} \n", recordMetadata.topic(), recordMetadata.partition(), recordMetadata.offset(), recordMetadata.timestamp());
-                    } else {
-                        logger.error("Error while sending message, {}", e.getMessage(), e);
-                    }
-                }
-            });
-        }
-    }
-
-    public void sendMessageWithKey(String key , String message){
-        try {
-            String topic = "first_topic";
-            Properties props = new Properties();
-            // create properties
-            props.put("bootstrap.servers", "127.0.0.1:9092");
-            props.put("key.serializer", StringSerializer.class.getName());
-            props.put("value.serializer", StringSerializer.class.getName());
-            // create producer
-            KafkaProducer<String, String> producer = new KafkaProducer<>(props);
-
-            for(int j =  0; j < 3; j++) {
-                for (int i = 0; i < 10; i++) {
-                    String key_ = key + i;
-                    String value_ = message + i;
-                    ProducerRecord<String, String> record = new ProducerRecord<>(topic, key_, value_);
-                    producer.send(record, new Callback() {
-                        @Override
-                        public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-                            // executes every time a record successfully sent or throw an exception
-                            if (e == null) {
-                                logger.info("Received new metadata \n" +
-                                        "Key : {} \n" +
-                                        "Partition : {} \n", key_, recordMetadata.partition());
-                            } else {
-                                logger.error("Error while sending message, {}", e.getMessage(), e);
-                            }
-                        }
-                    });
-                }
-            }
-        }
-        catch (Exception e){
-            logger.error("Error while sending message, {}", e.getMessage(), e);
-        }
-    }
 }
